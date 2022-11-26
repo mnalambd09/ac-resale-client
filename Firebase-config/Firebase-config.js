4789fd0be86f92a61b883e148365c63b3e21362d
mnalambd09 <mnalambd09@gmail.com>
2022-11-26 11:38:29 +0600

    navbar and ico file uploaded

---
+++ Firebase-config/Firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9lx15XxWY2Ni36qbB17fCXIX4fVqiDF8",
+  authDomain: "ac-resale-client.firebaseapp.com",
+  projectId: "ac-resale-client",
+  storageBucket: "ac-resale-client.appspot.com",
+  messagingSenderId: "581544573566",
+  appId: "1:581544573566:web:2723cff84ff2fd3448f62f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
