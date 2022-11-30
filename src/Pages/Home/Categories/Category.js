db8dc7fdfb9747dafbf33cd07db719f253724bd0
mnalambd09 <mnalambd09@gmail.com>
2022-11-30 13:45:54 +0600

    category section designed

--- src/Pages/Home/Categories/Category.js
+++ src/Pages/Home/Categories/Category.js
@@ -1,5 +1,5 @@
-import React, { Children, useContext, useEffect, useState } from 'react';
-import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import {  useNavigate, useParams } from 'react-router-dom';
 import { AuthContext } from '../../../Context/AuthProvider';
 import Products from './Products';
 
@@ -15,7 +15,7 @@ const Category = () => {
         fetch(`http://localhost:5000/product/${params.id}`)
             .then(res => res.json())
             .then(data => setProduct(data))
-    },[])
+    }, [])
 
     if(!user?.uid){
         return navigate('/category')
@@ -24,7 +24,7 @@ const Category = () => {
     return (
 
         <div>
-            <h1 className='text-5xl text-orange-600'>This is Category </h1>
+            <h1 className='text-5xl text-orange-600 mt-10 font-bold'>Please Choice Your Product from the List </h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-10 auto my-10'>
             {
                 product.map(pro => <Products
--- src/Pages/Home/Categories/Products.js
+++ src/Pages/Home/Categories/Products.js
@@ -4,14 +4,16 @@ const Products = ({ pro }) => {
     const { picture, price, resalePrice, sellerName, title, usesTime } = pro;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img className='h-96' src={picture} alt="Shoes" /></figure>
+            <figure><img className='h-96 p-5' src={picture} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
-                <p>{price}</p>
-                <p>{resalePrice}</p>
-                <p>{sellerName}</p>
-                <p>{usesTime}</p>
-                <div className="card-actions justify-end">
+                <div className='text-left pl-5 font-extrabold text-green-600'>
+                    <p>Price : {price}</p>
+                    <p>Resale Price : ${resalePrice}</p>
+                    <p>Seller Name : {sellerName}</p>
+                    <p>Used Time : {usesTime}</p>
+                </div>
+                <div className="card-actions justify-end -mt-12">
                     <button className="btn btn-primary">Order Now</button>
                 </div>
             </div>
